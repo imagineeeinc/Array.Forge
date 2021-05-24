20237dc9509b263fd17bd03cd746990a9747038b
Imagineee <imagineeelego@gmail.com>
2021-05-24 18:59:02 +0100

    added funcs

--- src/array.forge.js
+++ src/array.forge.js
@@ -4,7 +4,7 @@ class aForger {
     }
     add(end, ...arr) {
         let str = false
-        if (Array.isArray(arr) === true) {
+        if (Array.isArray(arr[0]) === true) {
             str = true
         }
         if (end == "end") {
@@ -19,15 +19,104 @@ class aForger {
             } else {
                 this.arr = arr.concat(this.arr)
             }
-        } else if (isNaN(end) === false) {
+        } else if (Number.isInteger(end) === true) {
             if (str === true) {
-                this.arr = arr[0].concat(this.arr)
+                for (var i=0;i < arr[0].length;i++) {
+                    this.arr.splice(end+i, 0, arr[0])[i]
+                }
             } else {
-                this.arr = this.arr.splice(end, end-1, arr)
+                for (var i=0;i < arr.length;i++) {
+                    this.arr.splice(end+i, 0, arr[i])
+                }
+            }
+        } else {
+            if (str === true) {
+                this.arr = this.arr.concat(arr[0])
+            } else {
+                this.arr = this.arr.concat(arr)
             }
         }
         return this.arr
-    } 
+    }
+    delete(end) {
+        if (end == "end") {
+            this.arr.pop()
+        } else if (end == "start") {
+            this.arr.shift()
+        } else if (Number.isInteger(end) === true) {
+            let d = this.arr.splice(end, 1)
+        } else {
+            this.arr.pop()
+        }
+        return this.arr
+    }
+    copy() {
+        return this.arr.splice()
+    }
+    length() {
+        return this.arr.length
+    }
+    asStr(sep) {
+        return this.arr.join(sep || ",")
+    }
+    fill(val, start, end) {
+        this.arr.fill(val, start || 0, end || this.arr.length)
+        return this.arr
+    }
+    includes(inc) {
+        return this.arr.includes(inc)
+    }
+    posOf(ele) {
+        return this.arr.indexOf(ele)
+    }
+    reverse() {
+        this.arr.reverse()
+        return this.arr
+    }
+    sort(fn) {
+        this.arr.sort(fn || undefined)
+        return this.arr
+    }
+    from(fr) {
+        this.arr = Array.from(fr)
+        return this.arr
+    }
+    filter(fn) {
+        let out = this.arr.filter(fn)
+        return out
+    }
+    map(fn) {
+        let out = this.arr.map(fn)
+        return out
+    }
+    reduce(fn) {
+        let out = this.arr.reduce(fn)
+        return out
+    }
+    some(fn) {
+        let out = this.arr.some(fn)
+        return out
+    }
+    find(fn) {
+        let out = this.arr.find(fn)
+        return out
+    }
+    every(fn) {
+        let out = this.arr.every(fn)
+        return out
+    }
+    at(loc) {
+        console.warn("at is a proposed methoed and not implemented as of 2021\nif you want to use it.\nyou need to use a polyfill like: https://github.com/es-shims/Array.prototype.at")
+        return this.arr.at(loc)
+    }
 }
 
-export { aForger as aForge }
+function mergeArray(...arr) {
+    let ar = arr[0]
+    for (var i=1;i < arr.length;i++) {
+        ar = ar.concat(arr[i])
+    }
+    return ar
+}
+
+export { aForger as aForge, mergeArray }
--- test/app.js
+++ test/app.js
@@ -1,9 +1,15 @@
-import { aForge as forgeable } from "../src/array.forge.js"
+import { aForge as forgeable, mergeArray } from "../src/array.forge.js"
 
 var arry = new forgeable("loo","li", "loo", "lum")
 
-arry.add("end", "lye", "low", "lee", "lum")
+arry.add(4, "lye", "low", "lee", "lum")
+
+console.log(mergeArray(arry.arr, arry.arr))
+
+Array.prototype.ash = function() {
+    return this
+}
 
 console.log(arry)
 
-
+arry.fill("looo")
